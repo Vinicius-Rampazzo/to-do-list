9f3d5f666b850946c5a8aa800d8cf7af9f6e9eb9
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2023-05-30 10:04:53 -0300

    fix: fixing bugs and the function to delete selected items

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ const botaoSalvarTarefas = document.querySelector('#salvar-tarefas');
 const botaoRemoveSelecionado = document.querySelector('#remover-selecionado');
 let filhoDaLista = document.createElement('li');
 const classeDoFilhoDaLista = document.querySelector('.list');
-
 // coloquei o .value, pois ele me da o valor do input, e assim consigo manipular o que fica dentro dele.
 
-botaoAdicionar.addEventListener('click', interacaoBotaoAdicionar);
+function mudandoBackground(evento) {
+  evento.target.classList.toggle('selected');
+}
+// o ".toggle" usado acima, serve para variar, por exemplo, como ele está numa função de click, então fica: se eu clicar em um item da lista, aparece a classe "selected", se eu clicar novamente, a classe some, ou seja, se não tiver a classe que coloquei no ".toggle" ele vai criar, porém se ja tiver, ele vai deletar.
+
+function selecionandoComDoisClicks(doisClicks) {
+  doisClicks.target.classList.toggle('completed');
+
+  botaoFinalizados.addEventListener('click', interacaoBotaoFinalizados);
+}
+// acima só precisei fazer a classe "completed" aparecer e desaparecer após os dois clicks, adiciondo no evento que criei dentro da função "interacaoBotaoAdicionar", e mudando algumas coisas no css atravez da classe "completed", ou seja, quando eu dar os dois clicks, se não tiver a classe completed, vai aparecer ao dar os dois clicks, e as alteraçoes feitas no css dentro dessa classe "completed" será ativada certinho de uma vez.
+
 function interacaoBotaoAdicionar() {
   const inputTexto = input.value;
   // ".value" serve aqui para pegar o que tem dentro do input
@@ -27,28 +37,9 @@ function interacaoBotaoAdicionar() {
   filhoDaLista.addEventListener('click', mudandoBackground);
   filhoDaLista.addEventListener('dblclick', selecionandoComDoisClicks);
 }
-
+botaoAdicionar.addEventListener('click', interacaoBotaoAdicionar);
 // acima, usei o addEventListener para a cada click, ativar a minha função (que está dentro do parenteses).
 
-function mudandoBackground(evento) {
-  const classSelected = document.querySelector('.selected');
-  evento.target.classList.toggle('selected');
-  if (classSelected) {
-    classSelected.classList.remove('selected');
-  }
-}
-
-// o ".toggle" usado acima, serve para variar, por exemplo, como ele está numa função de click, então fica: se eu clicar em um item da lista, aparece a classe "selected", se eu clicar novamente, a classe some, ou seja, se não tiver a classe que coloquei no ".toggle" ele vai criar, porém se ja tiver, ele vai deletar.
-
-function selecionandoComDoisClicks(doisClicks) {
-  doisClicks.target.classList.toggle('completed');
-
-  botaoFinalizados.addEventListener('click', interacaoBotaoFinalizados);
-}
-
-// acima só precisei fazer a classe "completed" aparecer e desaparecer após os dois clicks, adiciondo no evento que criei dentro da função "interacaoBotaoAdicionar", e mudando algumas coisas no css atravez da classe "completed", ou seja, quando eu dar os dois clicks, se não tiver a classe completed, vai aparecer ao dar os dois clicks, e as alteraçoes feitas no css dentro dessa classe "completed" será ativada certinho de uma vez.
-
-botaoApagar.addEventListener('click', interacaoBotaoApagar);
 function interacaoBotaoApagar() {
   const classeLista = document.querySelectorAll('.list');
 
@@ -58,11 +49,10 @@ function interacaoBotaoApagar() {
     }
   }
 }
-
+botaoApagar.addEventListener('click', interacaoBotaoApagar);
 // Acima, fiz o cirei um botao para limpar todas as tags <li> que tiver a classe "list" (que no caso são todas).
 
 function interacaoBotaoFinalizados() {
-  // console.log('ola mundo!');
   const classCompleted = document.querySelectorAll('.completed');
 
   if (classCompleted) {
@@ -71,8 +61,22 @@ function interacaoBotaoFinalizados() {
     }
   }
 }
-
 // Acima eu percorri meu array atravez de uma condição, para que se eu clicar no botão "Remover Finalizados" com o evento criado na função "selecionandoComDoisClicks", remover apenas os itens selecionados com a classe "completed".
+
+function interacaoBotaoRemoverSelecionados() {
+  const classSelecionado = document.querySelectorAll('.selected');
+
+  if (classSelecionado) {
+    for (let index = 0; index < classSelecionado.length; index += 1) {
+      classSelecionado[index].remove();
+    }
+  }
+}
+botaoRemoveSelecionado.addEventListener(
+  'click',
+  interacaoBotaoRemoverSelecionados
+);
+// aqui eu busquei meu botao com o queryselector la em cima nas constantes, e depois busquei a classe que fiz "selected", para que se tiver a classe selected, eu remover clacando no botao com o addEventListener
 
 function interacaoBotaoSalvarTarefas() {
   const tagLi = document.querySelectorAll('.list');
@@ -88,19 +92,4 @@ function interacaoBotaoSalvarTarefas() {
   }
 }
 botaoSalvarTarefas.addEventListener('click', interacaoBotaoSalvarTarefas);
-
 // fiz um for pra percorrer tudo o que tiver, (percorrer todas as tags li), para conseguir salvar todas.
-
-botaoRemoveSelecionado.addEventListener(
-  'click',
-  interacaoBotaoRemoverSelecionados
-);
-function interacaoBotaoRemoverSelecionados() {
-  const classSelecionado = document.querySelector('.selected');
-
-  if (classSelecionado) {
-    classSelecionado.remove();
-  }
-}
-
-// aqui eu busquei meu botao com o queryselector la em cima nas constantes, e depois busquei a classe que fiz "selected", para que se tiver a classe selected, eu remover clacando no botao com o addEventListener
