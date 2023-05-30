273a1ae34bbaa9dff939745d0aefe6b80fb3ab41
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2023-05-30 09:21:39 -0300

    feat: uploading old commits

--- script.js
+++ script.js
@@ -6,115 +6,101 @@ const botaoFinalizados = document.querySelector('#remover-finalizados');
 const botaoSalvarTarefas = document.querySelector('#salvar-tarefas');
 const botaoRemoveSelecionado = document.querySelector('#remover-selecionado');
 let filhoDaLista = document.createElement('li');
-let classeDoFilhoDaLista = document.querySelector('.list');
-
-
+const classeDoFilhoDaLista = document.querySelector('.list');
 
 // coloquei o .value, pois ele me da o valor do input, e assim consigo manipular o que fica dentro dele.
 
-
 botaoAdicionar.addEventListener('click', interacaoBotaoAdicionar);
 function interacaoBotaoAdicionar() {
-	let inputTexto = input.value;
-// ".value" serve aqui para pegar o que tem dentro do input
+  const inputTexto = input.value;
+  // ".value" serve aqui para pegar o que tem dentro do input
 
-	filhoDaLista = document.createElement('li');
-	filhoDaLista.classList.add('list');
-	lista.appendChild(filhoDaLista);
+  filhoDaLista = document.createElement('li');
+  filhoDaLista.classList.add('list');
+  lista.appendChild(filhoDaLista);
 
-// acima eu quis dizer que, a cada clique que eu der no "adicionar", vai criar uma tag <li> e dentro dela, pegar o valor com o .value, e atribuir nas listas criadas. 
-	filhoDaLista.innerText = inputTexto;
-	input.value = '';
+  // acima eu quis dizer que, a cada clique que eu der no "adicionar", vai criar uma tag <li> e dentro dela, pegar o valor com o .value, e atribuir nas listas criadas.
+  filhoDaLista.innerText = inputTexto;
+  input.value = '';
 
-// acima eu atribui um valor '' vazio, ou seja, assim que eu clicar no botao "adicionar", além de se juntar na lista o que eu escrevi, também vai substituir o que está escrito no input por uma str vazia.
-	filhoDaLista.addEventListener('click', mudandoBackground);
-	filhoDaLista.addEventListener('dblclick', selecionandoComDoisClicks);
+  // acima eu atribui um valor '' vazio, ou seja, assim que eu clicar no botao "adicionar", além de se juntar na lista o que eu escrevi, também vai substituir o que está escrito no input por uma str vazia.
+  filhoDaLista.addEventListener('click', mudandoBackground);
+  filhoDaLista.addEventListener('dblclick', selecionandoComDoisClicks);
 }
 
 // acima, usei o addEventListener para a cada click, ativar a minha função (que está dentro do parenteses).
 
-
-
 function mudandoBackground(evento) {
-	const classSelected = document.querySelector('.selected');
-	evento.target.classList.toggle('selected');
-	if (classSelected) {
-		classSelected.classList.remove('selected');
-	}
+  const classSelected = document.querySelector('.selected');
+  evento.target.classList.toggle('selected');
+  if (classSelected) {
+    classSelected.classList.remove('selected');
+  }
 }
 
 // o ".toggle" usado acima, serve para variar, por exemplo, como ele está numa função de click, então fica: se eu clicar em um item da lista, aparece a classe "selected", se eu clicar novamente, a classe some, ou seja, se não tiver a classe que coloquei no ".toggle" ele vai criar, porém se ja tiver, ele vai deletar.
 
+function selecionandoComDoisClicks(doisClicks) {
+  doisClicks.target.classList.toggle('completed');
 
-
-function selecionandoComDoisClicks (doisClicks) {
-
-	doisClicks.target.classList.toggle('completed');
-
-	botaoFinalizados.addEventListener('click', interacaoBotaoFinalizados);
+  botaoFinalizados.addEventListener('click', interacaoBotaoFinalizados);
 }
 
 // acima só precisei fazer a classe "completed" aparecer e desaparecer após os dois clicks, adiciondo no evento que criei dentro da função "interacaoBotaoAdicionar", e mudando algumas coisas no css atravez da classe "completed", ou seja, quando eu dar os dois clicks, se não tiver a classe completed, vai aparecer ao dar os dois clicks, e as alteraçoes feitas no css dentro dessa classe "completed" será ativada certinho de uma vez.
 
-
 botaoApagar.addEventListener('click', interacaoBotaoApagar);
 function interacaoBotaoApagar() {
-	const classeLista = document.querySelectorAll('.list');
+  const classeLista = document.querySelectorAll('.list');
 
-	if (classeLista) {
-		for (let index = 0; index < classeLista.length; index += 1) {
-			classeLista[index].remove();
-		}
-	}
+  if (classeLista) {
+    for (let index = 0; index < classeLista.length; index += 1) {
+      classeLista[index].remove();
+    }
+  }
 }
 
-// Acima, fiz o cirei um botao para limpar todas as tags <li> que tiver a classe "list" (que no caso são todas). 
-
-
+// Acima, fiz o cirei um botao para limpar todas as tags <li> que tiver a classe "list" (que no caso são todas).
 
 function interacaoBotaoFinalizados() {
-	// console.log('ola mundo!');
-	let classCompleted = document.querySelectorAll('.completed');
-	
-	if (classCompleted) {
-		for (let index = 0; index < classCompleted.length; index += 1) {
-			classCompleted[index].remove();
-		}
-	}
+  // console.log('ola mundo!');
+  const classCompleted = document.querySelectorAll('.completed');
+
+  if (classCompleted) {
+    for (let index = 0; index < classCompleted.length; index += 1) {
+      classCompleted[index].remove();
+    }
+  }
 }
 
 // Acima eu percorri meu array atravez de uma condição, para que se eu clicar no botão "Remover Finalizados" com o evento criado na função "selecionandoComDoisClicks", remover apenas os itens selecionados com a classe "completed".
 
-
-
-botaoSalvarTarefas.addEventListener('click', interacaoBotaoSalvarTarefas);
 function interacaoBotaoSalvarTarefas() {
+  const tagLi = document.querySelectorAll('.list');
+  const arrayItens = JSON.parse(localStorage.getItem('chaveItensSalvos'));
 
-	const tagLi = document.querySelectorAll('.list');
-	// const a = JSON.stringify(tagLiHTML);
-	if (tagLi) {
-		for (let index = 0; index < tagLi.length; index += 1) {
-			// console.log('oi')
-			const tagLiHTML = tagLi.innerHTML;
-			localStorage.setItem('chaveItensSalvos', JSON.stringify(tagLi));
-			// localStorage.setItem('chave', tagLiHTML)
-		}
-			
-	}	
-
+  if (tagLi) {
+    for (let index = 0; index < tagLi.length; index += 1) {
+      // console.log(tagLi)
+      arrayItens.push(tagLi[index].innerHTML);
+      localStorage.setItem('chaveItensSalvos', JSON.stringify(arrayItens));
+      // localStorage.setItem('chave', tagLiHTML)
+    }
+  }
 }
+botaoSalvarTarefas.addEventListener('click', interacaoBotaoSalvarTarefas);
 
 // fiz um for pra percorrer tudo o que tiver, (percorrer todas as tags li), para conseguir salvar todas.
 
-
-
-botaoRemoveSelecionado.addEventListener('click', interacaoBotaoRemoverSelecionados);
+botaoRemoveSelecionado.addEventListener(
+  'click',
+  interacaoBotaoRemoverSelecionados
+);
 function interacaoBotaoRemoverSelecionados() {
-	let classSelecionado = document.querySelector('.selected')
+  const classSelecionado = document.querySelector('.selected');
 
-	if (classSelecionado) {
-		classSelecionado.remove()
-	}
+  if (classSelecionado) {
+    classSelecionado.remove();
+  }
 }
 
 // aqui eu busquei meu botao com o queryselector la em cima nas constantes, e depois busquei a classe que fiz "selected", para que se tiver a classe selected, eu remover clacando no botao com o addEventListener
